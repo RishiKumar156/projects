67cc548ff0d32dcff81e9f934f664806f2a9b50a
Pharaoh_devil <rishikk156@gmail.com>
2023-12-26 20:52:35 +0530

    function based updation

--- pattern.py
+++ pattern.py
@@ -1,9 +1,10 @@
 
-
+pattern_size = int(input("Hi user how many patterns you wanna draw ?: "))
 def create_mario_pattern(n):
+    n+=1
     val = ""
-    for i in range(1, n+1):
-        val += '*'
+    for i in range(1, n-1):
+        val += '#'
         print(val)
-
-create_mario_pattern(5)
+if pattern_size : 
+    create_mario_pattern(pattern_size)    
