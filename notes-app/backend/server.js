61f7628be691a6424ec7cfd9b250e681c6dd7d7f
Pharaoh_devil <rishikk156@gmail.com>
2023-12-26 12:14:18 +0530

    notes application sample

--- notes-app/backend/server.js
+++ notes-app/backend/server.js
@@ -1,11 +1,23 @@
 const http = require('http');
-const url = require('url');
+
 const server = http.createServer((req, res) => {
-    const paths = req.url;
-    res.end('Server started')
-})
+    const mongodbURL = 'mongodb+srv://rishik:sagpenty@cluster0.opou9wy.mongodb.net/?retryWrites=true&w=majority';
+    switch (req.url) {
+        case '/':
+            res.end('You reached the home page');
+            break;
+        case '/notes':
+            res.end('You reached the notes page');
+            break;
+        case '/createNotes':
+            res.end('You reached the create notes page');
+            break;
+        default:
+            res.end('Page not found');
+            break;
+    }
+});
 
-
-server.listen(3000, '127.0.0.1' ,() => {
-    console.log('Server running on port 3000')
-})
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server running on port 3000');
+});
