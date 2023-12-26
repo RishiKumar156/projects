745f7b65b2ba3cb2226e58a81321313af135ce77
Pharaoh_devil <rishikk156@gmail.com>
2023-12-26 20:55:03 +0530

    code base updation

--- pattern.py
+++ pattern.py
@@ -1,9 +1,8 @@
 
 pattern_size = int(input("Hi user how many patterns you wanna draw ?: "))
 def create_mario_pattern(n):
-    n+=1
     val = ""
-    for i in range(1, n-1):
+    for i in range(1, n+1):
         val += '#'
         print(val)
 if pattern_size : 
